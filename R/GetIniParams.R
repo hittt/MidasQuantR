e1b00e2b5c8e4669f703b57fe592fa3b6b8dac83
TrungLeVn <trunglehai.hvnh@gmail.com>
2018-07-19 00:12:12 +0100

    Add beta2para argument to control for the choice of Beta Polynomial

--- R/GetIniParams.R
+++ R/GetIniParams.R
@@ -2,10 +2,10 @@
 # Function to get the initial parameters to start the optimization
 #-----------------------
 
-GetIniParams <- function(y, X, q, nlag, numInitialsRand = 10000, numInitials = 10){
+GetIniParams <- function(y, X, q, numInitialsRand = 10000, numInitials = 10,beta2para = FALSE){
   # Randomly sample second parameter of Beta polynomial
-  InitialParamsVec = GetIniParamsC(y,X,q,numInitialsRand,numInitials)
-  SortedResults = InitialParamsVec[order(InitialParamsVec[,1],decreasing = TRUE),]
-  beta = SortedResults[1:numInitials,2:5]
+  InitialParamsVec = GetIniParamsC(y,X,q,numInitialsRand,numInitials,beta2para)
+  SortedResults = InitialParamsVec[order(InitialParamsVec[,1]),]
+  beta = SortedResults[1:numInitials,2:(4 + beta2para)]
   return(beta)
 }
