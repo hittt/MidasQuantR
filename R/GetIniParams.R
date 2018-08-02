ea9dd4d09877fbb48576d4b24e5c8738f530f053
TrungLeVn <trunglehai.hvnh@gmail.com>
2018-08-02 13:56:03 +0100

    Adding the function to get initial paramters for CAViaR model

--- R/GetIniParams.R
+++ R/GetIniParams.R
@@ -2,26 +2,47 @@
 # Function to get the initial parameters to start the optimization
 #-----------------------
 
+#-------------------------------------------------------
+# Get initial parameters for the univariate midas quantile
+#-------------------------------------------------------
+
 #' @export
-GetIniParams <- function(y, X, X_neg, X_pos, q, numInitialsRand = 10000, 
+GetIniParams_midas <- function(y, X, X_neg, X_pos, q, numInitialsRand = 10000, 
                          numInitials = 10,beta2para = FALSE, As = FALSE){
   # Randomly sample second parameter of Beta polynomial
-  InitialParamsVec = GetIniParamsUni(y,X,X_neg, X_pos, q,numInitialsRand,numInitials,beta2para,As)
+  InitialParamsVec = C_GetIniParams_midas(y,X,X_neg, X_pos, q,numInitialsRand,numInitials,beta2para,As)
   SortedResults = InitialParamsVec[order(InitialParamsVec[,1]),]
   beta = SortedResults[1:numInitials,2:(3 + As + beta2para + 1)]
   return(beta)
 }
 
+#------------------------------------------------------------
+# Get initial parameters for the midas quantile with AL distribution
+#------------------------------------------------------------
+
 #' @export
-GetIniParams_AL <- function(y, condMean, QuantEst, X, X_neg, X_pos, q,
+GetIniParams_midasAL <- function(y, condMean, QuantEst, X, X_neg, X_pos, q,
                             numInitialsRand = 10000, numInitials = 10,
                             beta2para = FALSE, As = FALSE){
   # Randomly sample second parameter of Beta polynomial
-  InitialParamsVec = GetIniParamsAL(yr = y, condmeanR = condMean, QuantEst = QuantEst,
+  InitialParamsVec = C_GetIniParams_midasAL(yr = y, condmeanR = condMean, QuantEst = QuantEst,
                                     Xr = X, Xr_neg = X_neg,Xr_pos = X_pos, q = q,
                                     numInitialsRand = numInitialsRand, numInitials = numInitials,
                                     beta2para = beta2para, As = As)
-  SortedResults = InitialParamsVec#[order(InitialParamsVec[,1]),]
+  SortedResults = InitialParamsVec[order(InitialParamsVec[,1]),]
   beta = SortedResults[1:numInitials,2:(4 + As + beta2para + 1)]
+  return(beta)
+}
+
+#-------------------------------------------------------------
+# Get initial parameters for the CAViaR model
+#-------------------------------------------------------------
+#' @export
+
+GetIniParams_cav <- function(y, x, q, model, empQuant, Uni = TRUE, numInitialsRand = 10000, numInitials = 10){
+  InitialParamsVec = C_GetIniParams_cav(yr = y, Xr = x, q = q, model = model, empQuant = empQuant,
+                                        Uni = Uni, numInitialsRand = numInitialsRand, numInitials = numInitials)
+  SortedResults = InitialParamsVec[order(InitialParamsVec[,1]),]
+  beta = SortedResults[1:numInitials,2:(2 + model + 1)]
   return(beta)
 }
